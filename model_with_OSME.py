5006021e7457ab317b839ac6d1dfabd5a0194c6a
xcnkx <cnk_2806@me.com>
2018-10-30 09:02:53 +0000

    add OSME module

--- model_with_OSME.py
+++ model_with_OSME.py
@@ -8,9 +8,9 @@ Created on Tue Oct 23 08:45:20 2018
 
 from keras.callbacks import EarlyStopping
 from keras.applications.resnet50 import ResNet50
-from keras.models import Sequential, Model
-from keras.layers import Input, Dense, Dropout, Activation, Flatten, Multiply
-from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, AveragePooling2D
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten, Multiply, add
+from keras.layers import GlobalAveragePooling2D
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau
 from keras.optimizers import SGD
 from keras.utils.np_utils import to_categorical
@@ -75,33 +75,36 @@ input_tensor = Input(shape=(img_size, img_size, 3))
 base_model = ResNet50(weights = "imagenet", include_top=False, input_tensor=input_tensor)
 
 
-#for layer in base_model.layers:
-#    layer.trainable = False
+for layer in base_model.layers:
+    layer.trainable = False
     
-#base_model.summary()
 #%% Implementation of OSME module
-#
-#def osme_block(in_block, ch, ratio=16):
-#    z = GlobalAveragePooling2D()(in_block) # 1
-#    x = Dense(ch//ratio, activation='relu')(z) # 2
-#    x = Dense(ch, activation='sigmoid')(x) # 3
-#    return Multiply([in_block, x]) # 4
+
+def osme_block(in_block, ch, ratio=16):
+    z = GlobalAveragePooling2D()(in_block) # 1
+    x = Dense(ch//ratio, activation='relu')(z) # 2
+    x = Dense(ch, activation='sigmoid')(x) # 3
+    return Multiply()([in_block, x]) # 4
+
+s_1 = osme_block(base_model.output, base_model.output_shape[3])
+s_2 = osme_block(base_model.output, base_model.output_shape[3])
+
+fc1 = Dense(1024, name='fc1')(s_1)
+fc2 = Dense(1024, name='fc2')(s_2)
+
+fc1 = Flatten()(fc1)
+fc2 = Flatten()(fc2)
+
+fc = add([fc1,fc2]) # fc1 + fc2
+
+prediction = Dense(num_classes, activation='softmax', name='prediction')(fc)
 
 
-#%%
-# change only the output layer 
-top_model = Sequential()
-#top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
-top_model.add(GlobalAveragePooling2D(input_shape=base_model.output_shape[1:]))
-top_model.add(Dense(512,activation='relu'))
-top_model.add(Dropout(0.5))
-top_model.add(Dense(num_classes, activation='softmax'))
-
-model = Model(input=base_model.input, output=top_model(base_model.output))
+model = Model(input=base_model.input, output=prediction)
 
 opt = SGD(lr=0.00001, momentum=0.9)
 
-model.load_weights("/home/n-kamiya/models/model_without_MAMC/model_alpha.best_loss.hdf5")
+#model.load_weights("/home/n-kamiya/models/model_without_MAMC/model_alpha.best_loss.hdf5")
     
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
@@ -109,7 +112,7 @@ model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy
 plot_model(model, to_file="model.png", show_shapes=True)
 
 #%% implement checkpointer and reduce_lr (to prevent overfitting)
-checkpointer = ModelCheckpoint(filepath='/home/n-kamiya/models/model_without_MAMC/model_alpha.best_loss.hdf5', verbose=1, save_best_only=True)
+checkpointer = ModelCheckpoint(filepath='/home/n-kamiya/models/model_without_MAMC/model_osme.best_loss.hdf5', verbose=1, save_best_only=True)
 
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
                   patience=5, min_lr=0.000001)
@@ -136,7 +139,7 @@ plt.title('model_without_MAMC accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.savefig("/home/n-kamiya/models/model_without_MAMC/history_{0:%d%m}-{0:%H%M%S}.png".format(now))
+plt.savefig("/home/n-kamiya/models/model_without_MAMC/history_osme_{0:%d%m}-{0:%H%M%S}.png".format(now))
 plt.show()
 
 #loss
@@ -146,5 +149,5 @@ plt.title('model_without_MAMC loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.savefig("/home/n-kamiya/models/model_without_MAMC/loss_{0:%d%m}-{0:%H%M%S}.png".format(now))
+plt.savefig("/home/n-kamiya/models/model_without_MAMC/loss_osme_{0:%d%m}-{0:%H%M%S}.png".format(now))
 plt.show()
